80ab051bfcaa1871e1ae6bf278114465ecc5c843
Keshav Bahadoor <keshav.bahadoor@gmail.com>
2017-12-09 23:47:17 -0400

    Implemented config service

---
+++ MyHealthPassAuth/Services/ConfigService.cs
@@ -0,0 +1,128 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyHealthPassAuth.Entities;
+using MyHealthPassAuth.Repository;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace MyHealthPassAuth.Services
+{
+    /// <summary>
+    /// This will enable access to authorization configurations (Example: 
+    ///     password length configs, allowed password size, etc.) 
+    /// Ideally, this should use some sort of configuration mamangement, 
+    /// but the database context will do for now. 
+    /// </summary>
+    public class ConfigService
+    {
+        /// <summary>
+        /// Local private instance 
+        /// </summary>
+        private static readonly ConfigService _instance = new ConfigService();
+
+        /// <summary>
+        /// List of all configurations available 
+        /// </summary>
+        private List<AuthorizationConfig> _configs;
+        public List<AuthorizationConfig> Configs
+        {
+            get { return _configs; }            
+        }
+
+        /// <summary>
+        /// Represents the time that the configurtions was loaded. 
+        /// The idea here is to only read configs from database into memory infrequently, and not 
+        /// at each request. 
+        /// </summary>
+        private DateTime _lastConfigListUpdate; 
+
+        /// <summary>
+        /// Database context that exposes repository
+        /// </summary>
+        private MainDbContext _dbContext;
+        public MainDbContext DbContext
+        {
+            get { return _dbContext; }
+            set { _dbContext = value; }
+        }
+
+        private UnitOfWork _dbUnitOfWork;
+
+        private ConfigService()
+        {
+            _configs = new List<AuthorizationConfig>();
+        }
+
+        /// <summary>
+        /// Thread safe singleton implementation 
+        /// </summary>
+        public static ConfigService Instance
+        {
+            get { return _instance; }
+        }
+
+        /// <summary>
+        /// Requires a database context options object for usage. 
+        /// </summary>
+        /// <param name="dbContextOptions"></param>
+        public void Initialize(DbContextOptions<MainDbContext> dbContextOptions)
+        {
+            _dbContext = new MainDbContext(dbContextOptions);
+            _dbUnitOfWork = new UnitOfWork(_dbContext);
+            LoadConfigurationList(); 
+        }
+
+        /// <summary>
+        /// Creates a local configuration list from database. 
+        /// </summary>
+        public void LoadConfigurationList()
+        {
+            try
+            {
+                if (_dbContext == null)
+                {
+                    throw new Exception("Database context not initialized");
+                }
+
+                this._configs.Clear(); 
+                this._configs = _dbUnitOfWork.AuthConfigRepository.Entities.ToList(); 
+                this._lastConfigListUpdate = DateTime.Now;
+            }
+            catch(Exception ex)
+            {
+                // TODO : some logging would be nice
+            }            
+        }
+
+        /// <summary>
+        /// Returns a configuration object for a specified location ID.
+        /// If a location is not found, returns null. 
+        /// </summary>
+        /// <param name="locationID"></param>
+        /// <returns></returns>
+        public AuthorizationConfig GetConfigForLocationID(int locationID)
+        {
+            try
+            {
+                if (_dbContext == null)
+                {
+                    throw new Exception("Database context not initialized");
+                }
+
+                foreach(AuthorizationConfig config in _configs)
+                {
+                    if (config.LocationID == locationID)
+                        return config; 
+                }
+                return null; 
+            }
+            catch(Exception ex)
+            {
+                // TODO : some logging would be nice
+                return null; 
+            }
+        }
+         
+    }
+}
---
+++ MyHealthPassAuthTest/ConfigServiceTest.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyHealthPassAuth.Repository;
+using MyHealthPassAuth.Entities;
+using System;
+using System.Linq;
+using MyHealthPassAuth.Services;
+using System.Collections.Generic;
+
+namespace MyHealthPassAuthTest
+{
+    [TestClass]
+    public class ConfigServiceTest
+    { 
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            DbContextOptions<MainDbContext> inMemoryOptions = new DbContextOptionsBuilder<MainDbContext>()
+               .UseInMemoryDatabase(databaseName: "in_memory_db_config")
+               .EnableSensitiveDataLogging(true) 
+               .Options;
+
+            ConfigService.Instance.Initialize(inMemoryOptions);
+            ConfigService.Instance.DbContext.Database.EnsureDeleted();
+        }
+
+        [TestCleanup]
+        public void TearDown()
+        { 
+            ConfigService.Instance.DbContext.Database.EnsureDeleted();
+        }
+
+        [TestMethod]
+        public void TestLocalConfigListContent()
+        {
+            TestUtils.AddAuthConfig(ConfigService.Instance.DbContext, 1, 15, 5, 1, 1, 1, 1, 123, 3, 13, 600, 1200, 1);
+            TestUtils.AddAuthConfig(ConfigService.Instance.DbContext, 2, 20, 15, 1, 1, 1, 1, 1000, 3, 14, 1600, 1200, 2);
+            TestUtils.AddAuthConfig(ConfigService.Instance.DbContext, 3, 11, 5, 2, 1, 1, 1, 100, 3, 13, 600, 1200, 3);
+            TestUtils.AddAuthConfig(ConfigService.Instance.DbContext, 4, 22, 5, 1, 2, 1, 1, 10000, 3, 15, 600, 1200, 4);
+            TestUtils.AddAuthConfig(ConfigService.Instance.DbContext, 5, 12, 5, 1, 1, 2, 1, 10000, 3, 13, 100, 1200, 5);
+            TestUtils.AddAuthConfig(ConfigService.Instance.DbContext, 6, 15, 5, 1, 2, 1, 1, 10000, 3, 13, 1600, 1200, 6);
+
+            ConfigService.Instance.LoadConfigurationList();
+
+            CollectionAssert.AreEqual(ConfigService.Instance.Configs,
+                ConfigService.Instance.DbContext.AuthorizationConfig.ToList());
+        }
+
+        [TestMethod]
+        public void TestGetConfigsForLocationID()
+        {
+            LoadLocationsAndConfigs();
+
+            AuthorizationConfig config = ConfigService.Instance.GetConfigForLocationID(2);
+             
+            Assert.AreEqual(10, config.PasswordLengthMax);
+            Assert.AreEqual(2, config.PasswordLengthMin);
+            Assert.AreEqual(2, config.PasswordAllowedUppercaseCount);
+            Assert.AreEqual(4, config.PasswordAllowedLowercaseCount);
+            Assert.AreEqual(2, config.PasswordAllowedDigitCount);
+            Assert.AreEqual(1, config.PasswordAllowedSpecialCharCount);
+            Assert.AreEqual(10000, config.MaxUserSessionSeconds);
+            Assert.AreEqual(2, config.LoginAccountLockAttempts);
+            Assert.AreEqual(10, config.BruteForceBlockAttempts);
+            Assert.AreEqual(100, config.BruteForceIdentificationSeconds);
+            Assert.AreEqual(1200, config.BruteForceBlockSeconds);
+            Assert.AreEqual(2, config.LocationID);
+        }
+
+        [TestMethod]
+        public void TestGetConfigsForLocationIDNull()
+        {
+            LoadLocationsAndConfigs();
+
+            AuthorizationConfig config = ConfigService.Instance.GetConfigForLocationID(4);
+
+            Assert.IsNull(config);
+        }
+
+        private void LoadLocationsAndConfigs()
+        {
+            // Add some locations 
+            TestUtils.AddLocation(ConfigService.Instance.DbContext, 1, "Trinidad", "Chaguanas");
+            TestUtils.AddLocation(ConfigService.Instance.DbContext, 2, "United Kingdom", "London");
+            TestUtils.AddLocation(ConfigService.Instance.DbContext, 3, "Australia", "Melbourne");
+
+            // Add some auth configs 
+            TestUtils.AddAuthConfig(ConfigService.Instance.DbContext, 1, 15, 5, 1, 1, 1, 1, 123, 3, 13, 600, 1200, 1);
+            TestUtils.AddAuthConfig(ConfigService.Instance.DbContext, 2, 10, 2, 2, 4, 2, 1, 10000, 2, 10, 100, 1200, 2);
+            TestUtils.AddAuthConfig(ConfigService.Instance.DbContext, 3, 11, 5, 2, 1, 1, 1, 100, 3, 13, 500, 1200, 3);
+
+            ConfigService.Instance.LoadConfigurationList();
+        }
+    }
+}
