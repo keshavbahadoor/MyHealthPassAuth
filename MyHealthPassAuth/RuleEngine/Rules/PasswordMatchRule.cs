d618566f083a07c74e91cd21648b82f1922d7f04
Keshav Bahadoor <keshav.bahadoor@gmail.com>
2017-12-10 19:14:37 -0400

    Implemented PasswordMatch rule

--- MyHealthPassAuth/RuleEngine/Rules/PasswordMatchRule.cs
+++ MyHealthPassAuth/RuleEngine/Rules/PasswordMatchRule.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Text;
 using MyHealthPassAuth.Entities;
 using MyHealthPassAuth.System;
+using MyHealthPassAuth.Services;
 
 namespace MyHealthPassAuth.RuleEngine.Rules
 {
@@ -11,7 +12,26 @@ namespace MyHealthPassAuth.RuleEngine.Rules
     {
         public override Message EvaluateRuleHook(string username, string password, AuthorizationConfig configs, User user)
         {
-             
+            if (user == null)
+            {
+                return new Message
+                {
+                    Result = MessageResult.ERROR,
+                    Text = "User object not passed"
+                };
+            }
+
+            // TODO : use decryption here. 
+            
+            if (!password.Equals(user.Password))
+            {
+                DataService.Instance.IncrementUserFailedLoginAttempts(user); 
+                return new Message
+                {
+                    Result = MessageResult.ERROR,
+                    Text = "Incorrect Password"
+                };
+            }
             return _defaultSuccessMessage;
         }
     }
---
+++ MyHealthPassAuthTest/RulesTest/PasswordMatchRuleTest.cs
@@ -0,0 +1,110 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyHealthPassAuth.Repository;
+using MyHealthPassAuth.Entities;
+using System;
+using System.Linq;
+using MyHealthPassAuth.RuleEngine.Rules;
+using MyHealthPassAuth.System;
+using MyHealthPassAuth.Services;
+
+namespace MyHealthPassAuthTest.RulesTest
+{
+    [TestClass]
+    public class PasswordMatchRuleTest : AbstractRepositoryTest
+    {
+        private AuthorizationConfig config; 
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            Initialize(); 
+
+            config = new AuthorizationConfig
+            {
+                AuthorizationConfigID = 1,
+                PasswordLengthMax = 10,
+                PasswordLengthMin = 1,
+                PasswordAllowedUppercaseCount = 1,
+                PasswordAllowedLowercaseCount = 1,
+                PasswordAllowedDigitCount = 1,
+                PasswordAllowedSpecialCharCount = 1,
+                LoginAccountLockAttempts = 3,
+                BruteForceBlockAttempts = 13,
+                BruteForceIdentificationSeconds = 600,
+                BruteForceBlockSeconds = 600,
+                LocationID = 1
+            };
+
+            TestUtils.AddUserToRepository(testDbContext,
+                1,
+                "keshav",
+                "password123",
+                0,
+                1,
+                new DateTime(2017, 12, 9, 0, 0, 0));
+
+            DataService.Instance.Initialize(this.testDbContext);
+        }
+
+        [TestCleanup]
+        public void TestTearDown()
+        {
+            TearDown(); 
+        }
+
+        [TestMethod]
+        public void TestPasswordMatchError()
+        {
+            PasswordMatchRule rule = new PasswordMatchRule();
+
+            Message result = rule.EvaluateRule("keshav", "qwerty123", config, testDbContext.Users.Single());            
+
+            // Separate instance of context to verify data insert 
+            using (var context = new MainDbContext(inMemoryOptions))
+            {
+                Assert.AreEqual(MessageResult.ERROR, result.Result);
+                Assert.AreEqual("Incorrect Password", result.Text);
+                Assert.AreEqual(1, context.Users.Single().FailedLoginAttempts); 
+            }
+        }
+
+        [TestMethod]
+        public void TestPasswordMatchErrorMultiple()
+        {
+            PasswordMatchRule rule = new PasswordMatchRule();
+
+            // attempt 1 
+            Message result = rule.EvaluateRule("keshav", "qwerty123", config, testDbContext.Users.Single());
+
+            // attempt 2 
+            result = rule.EvaluateRule("keshav", "zxcvbn", config, testDbContext.Users.Single());
+
+            // attempt 3 
+            result = rule.EvaluateRule("keshav", "pokemon123", config, testDbContext.Users.Single());
+
+            // Separate instance of context to verify data insert 
+            using (var context = new MainDbContext(inMemoryOptions))
+            {
+                Assert.AreEqual(MessageResult.ERROR, result.Result);
+                Assert.AreEqual("Incorrect Password", result.Text);
+                Assert.AreEqual(3, context.Users.Single().FailedLoginAttempts);
+            }
+        }
+
+        [TestMethod]
+        public void TestPasswordMatchSuccess()
+        {
+            PasswordMatchRule rule = new PasswordMatchRule();
+
+            Message result = rule.EvaluateRule("keshav", "password123", config, testDbContext.Users.Single());
+
+            // Separate instance of context to verify data insert 
+            using (var context = new MainDbContext(inMemoryOptions))
+            {
+                Assert.AreEqual(MessageResult.SUCCESS, result.Result); 
+                Assert.AreEqual(0, context.Users.Single().FailedLoginAttempts);
+            }
+        }
+    }
+}
