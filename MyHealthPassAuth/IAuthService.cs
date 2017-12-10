d676f8afde2e02fe5af530681a0ae437e2eb950f
Keshav Bahadoor <keshav.bahadoor@gmail.com>
2017-12-10 10:32:54 -0400

    Implemented entrypoint object for library

---
+++ MyHealthPassAuth/IAuthService.cs
@@ -0,0 +1,34 @@
+﻿using MyHealthPassAuth.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyHealthPassAuth
+{
+    public interface IAuthService
+    {
+        /// <summary>
+        /// Authenticates a user using the provided username, password. 
+        /// Returns a user object. 
+        /// </summary>
+        /// <param name="username"></param>
+        /// <param name="password"></param>
+        /// <param name="userAgent"></param>
+        /// <param name="requestData"></param>
+        /// <returns></returns>
+        User Login(string username, string password, string userAgent, string requestData);
+
+        /// <summary>
+        /// Performs a password check using password policy requirements deletgated to a 
+        /// specific location. 
+        /// </summary>
+        /// <param name="password"></param>
+        /// <param name="location"></param>
+        /// <returns></returns>
+        bool CheckPassword(string password, Location location);
+
+        string EncryptPassword(string password);
+
+        string DecryptPassword(string password);
+    }
+}
---
+++ MyHealthPassAuthTest/MyHealthPassAuthTest.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyHealthPassAuth.Repository;
+using MyHealthPassAuth.Entities;
+using System;
+using System.Linq;
+
+namespace MyHealthPassAuthTest
+{
+    [TestClass]
+    public class MyHealthPassAuthTest
+    {
+        [TestMethod]
+        public void TestCheckPassword()
+        {
+
+        }
+
+        [TestMethod]
+        public void TestLogin()
+        {
+
+        }
+
+        [TestMethod]
+        public void TestEncryptPassword()
+        {
+
+        }
+
+        [TestMethod]
+        public void TestDecryptPassword()
+        {
+
+        }
+    }
+}
