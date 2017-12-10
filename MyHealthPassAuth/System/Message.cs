e0dede56af27e0329234959fcd16242755d23197
Keshav Bahadoor <keshav.bahadoor@gmail.com>
2017-12-10 10:40:11 -0400

    Added system message class

---
+++ MyHealthPassAuth/System/Message.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyHealthPassAuth.System
+{
+    public enum MessageResult
+    {
+        SUCCESS = 1, 
+        ERROR = 2
+    }
+
+    /// <summary>
+    /// Encapsulates messages among system 
+    /// </summary>
+    public struct Message
+    {
+        private MessageResult messageResult; 
+        public MessageResult Result
+        {
+            get { return messageResult; }
+            set { messageResult = value; }
+        }
+
+        private string text; 
+        public string Text
+        {
+            get { return text; }
+            set { text = value; }
+        }
+    }
+}
