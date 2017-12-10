5adb558f6d5446783a94d7685e937edc59fd1f13
Keshav Bahadoor <keshav.bahadoor@gmail.com>
2017-12-10 16:11:14 -0400

    Added rules handler implementation.

--- MyHealthPassAuth/RuleEngine/RulesHandler.cs
+++ MyHealthPassAuth/RuleEngine/RulesHandler.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using MyHealthPassAuth.Entities;
+using MyHealthPassAuth.RuleEngine.Interfaces;
+using MyHealthPassAuth.RuleEngine.Rules;
+using MyHealthPassAuth.Services;
+using MyHealthPassAuth.System;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -10,10 +15,84 @@ namespace MyHealthPassAuth.RuleEngine
         /// This will contain a list of rule classes to apply to object. 
         /// TODO : Can move this to persistence layer 
         /// </summary>
-        private static readonly List<string> _loginAuthRules = new List<string>
+        private static readonly List<IAuthRule> _loginAuthRules = new List<IAuthRule>
         { 
+            new PasswordLengthRule()
         }; 
 
 
+        /// <summary>
+        /// Evaluates all required login rules on the given username and password. 
+        /// Note: We are relying on getting the Location ID from the user object in this case. 
+        /// </summary>
+        /// <param name="username"></param>
+        /// <param name="password"></param>
+        /// <returns></returns>
+        public Message HandleLogin(string username, string password)
+        {
+            try
+            {
+                User user = DataService.Instance.FindUserByUsername(username); 
+                if (user == null)
+                {
+                    return new Message
+                    {
+                        Result = MessageResult.ERROR, 
+                        Text = "User does not exist"
+                    };
+                }
+                AuthorizationConfig config = ConfigService.Instance.GetConfigForLocationID(user.LocationID);
+                return EvaluateRules(_loginAuthRules, username, password, config); 
+            }
+            catch(Exception ex)
+            {
+                // TODO : Some logging would be nice 
+
+                return new Message
+                {
+                    Result = MessageResult.ERROR,
+                    Text = "Error: " + ex.Message
+                };
+            }
+        }
+
+        /// <summary>
+        /// Iterates through a list of rules and evaluate each one. 
+        /// If an error occurs, the operation is broken and the Message object returned. 
+        /// </summary>
+        /// <param name="rules"></param>
+        /// <param name="username"></param>
+        /// <param name="password"></param>
+        /// <param name="configs"></param>
+        /// <returns></returns>
+        private Message EvaluateRules(List<IAuthRule> rules, string username, string password, AuthorizationConfig configs)
+        {
+            try
+            { 
+                foreach (IAuthRule rule in rules)
+                {
+                    Message message = rule.EvaluateRule(username, password, configs);
+                    if (message.Result == MessageResult.ERROR)
+                    {
+                        return message; 
+                    }
+                }
+                return new Message
+                {
+                    Result = MessageResult.SUCCESS,
+                    Text = "success"
+                };
+            }
+            catch(Exception ex)
+            {
+                // TODO : Some logging would be nice 
+
+                return new Message
+                {
+                    Result = MessageResult.ERROR,
+                    Text = "Error: " + ex.Message
+                };
+            }
+        }
     }
 }
---
+++ MyHealthPassAuthTest/RulesTest/RulesHandlerTest.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyHealthPassAuth.Repository;
+using MyHealthPassAuth.Entities;
+using System;
+using System.Linq;
+using MyHealthPassAuth.RuleEngine.Rules;
+using MyHealthPassAuth.System;
+using MyHealthPassAuth.Services;
+using MyHealthPassAuth.RuleEngine;
+
+namespace MyHealthPassAuthTest.RulesTest
+{
+    [TestClass]
+    public class RulesHandlerTest : AbstractRepositoryTest
+    {
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            Initialize();
+            DataService.Instance.Initialize(this.testDbContext);
+            ConfigService.Instance.Initialize(this.testDbContext);
+        }
+
+        [TestCleanup]
+        public void TestTearDown()
+        {
+            TearDown(); 
+        }
+
+        [TestMethod]
+        public void TestUserDoesNotExist()
+        {
+            TestUtils.AddUserToRepository(testDbContext,
+                1,
+                "test.user",
+                "123456",
+                0,
+                1,
+                new DateTime(2017, 12, 9, 0, 0, 0));
+
+            RulesHandler rulesHandler = new RulesHandler();
+            Message message = rulesHandler.HandleLogin("keshav.bahadoor", "123456");
+
+            Assert.AreEqual(MessageResult.ERROR, message.Result);
+            Assert.AreEqual("User does not exist", message.Text);
+        }
+
+        
+    }
+}
