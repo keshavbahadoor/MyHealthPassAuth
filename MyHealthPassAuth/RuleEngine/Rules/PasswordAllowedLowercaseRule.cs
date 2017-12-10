a48a03248e92f32a867e0e6521fa455a31caee78
Keshav Bahadoor <keshav.bahadoor@gmail.com>
2017-12-10 17:26:24 -0400

    Implemented Lowercase check rule

---
+++ MyHealthPassAuth/RuleEngine/Rules/PasswordAllowedLowercaseRule.cs
@@ -0,0 +1,27 @@
+﻿using MyHealthPassAuth.RuleEngine.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using MyHealthPassAuth.Entities;
+using MyHealthPassAuth.System;
+using System.Text.RegularExpressions;
+
+namespace MyHealthPassAuth.RuleEngine.Rules
+{
+    public class PasswordAllowedLowercaseRule : AbstractRule
+    { 
+        public override Message EvaluateRuleHook(string username, string password, AuthorizationConfig configs)
+        {
+            if (configs.PasswordAllowedLowercaseCount > Regex.Matches(password, @"\p{Ll}").Count) 
+            {
+                return new Message
+                {
+                    Result = MessageResult.ERROR,
+                    Text = string.Format("Password must contain {0} lowercase characters", 
+                                        configs.PasswordAllowedLowercaseCount)
+                };
+            }
+            return _defaultSuccessMessage;
+        }
+    }
+}
---
+++ MyHealthPassAuthTest/RulesTest/PasswordAllowedLowercaseRuleTest.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyHealthPassAuth.Repository;
+using MyHealthPassAuth.Entities;
+using System;
+using System.Linq;
+using MyHealthPassAuth.RuleEngine.Rules;
+using MyHealthPassAuth.System;
+
+namespace MyHealthPassAuthTest.RulesTest
+{
+    [TestClass]
+    public class PasswordAllowedLowercaseRuleTest
+    {
+        private AuthorizationConfig config;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            config = new AuthorizationConfig
+            {
+                AuthorizationConfigID = 1,
+                PasswordLengthMax = 10,
+                PasswordLengthMin = 1,
+                PasswordAllowedUppercaseCount = 2,
+                PasswordAllowedLowercaseCount = 2,
+                PasswordAllowedDigitCount = 1,
+                PasswordAllowedSpecialCharCount = 1,
+                LoginAccountLockAttempts = 3,
+                BruteForceBlockAttempts = 13,
+                BruteForceIdentificationSeconds = 600,
+                BruteForceBlockSeconds = 600,
+                LocationID = 1
+            };
+        }
+
+        [TestMethod]
+        public void TestNotEnoughLowercaseChars()
+        {
+            PasswordAllowedLowercaseRule rule = new PasswordAllowedLowercaseRule();
+            Message result = rule.EvaluateRule("keshav", "QWeTY123", config);
+
+            Assert.AreEqual(MessageResult.ERROR, result.Result);
+            Assert.AreEqual("Password must contain 2 lowercase characters", result.Text);
+        }
+
+        [TestMethod]
+        public void TestEnoughLowercaseChars()
+        {
+            PasswordAllowedLowercaseRule rule = new PasswordAllowedLowercaseRule();
+            Message result = rule.EvaluateRule("keshav", "abceQWERTY123", config);
+
+            Assert.AreEqual(MessageResult.SUCCESS, result.Result);
+        }
+
+        
+    }
+}
