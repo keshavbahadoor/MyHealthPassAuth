21ccc9ed312aa2596ca5b81fb324424cf6d5b738
Keshav Bahadoor <keshav.bahadoor@gmail.com>
2017-12-11 12:09:14 -0400

    Fixed rules handler

--- MyHealthPassAuth/RuleEngine/RulesHandler.cs
+++ MyHealthPassAuth/RuleEngine/RulesHandler.cs
@@ -3,6 +3,7 @@ using MyHealthPassAuth.RuleEngine.Interfaces;
 using MyHealthPassAuth.RuleEngine.Rules;
 using MyHealthPassAuth.Services;
 using MyHealthPassAuth.System;
+using MyHealthPassAuth.System.Interfaces;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -12,21 +13,90 @@ namespace MyHealthPassAuth.RuleEngine
     public class RulesHandler
     {
         /// <summary>
-        /// This will contain a list of rule classes to apply to object. 
-        /// TODO : Can move this to persistence layer 
+        /// This will contain a list of rule classes to apply during password check events.  
         /// </summary>
-        private static readonly List<IAuthRule> _loginAuthRules = new List<IAuthRule>
-        { 
-            new PasswordLengthRule()
-        }; 
+        private List<IAuthRule> _passwordCheckRules = new List<IAuthRule>();  
 
+        /// <summary>
+        /// This will contain a list of rules to apply during login events.   
+        /// </summary>
+        private List<IAuthRule> _loginRules = new List<IAuthRule>();  
+
+        private RequestData requestData;
+
+        private ILog Logger; 
+
+        public RulesHandler(RequestData data)
+        {
+            this.requestData = data;
+            CompileRules();
+        }
+
+        public RulesHandler(ILog logger, RequestData data)
+            : this(data)
+        {
+            this.Logger = logger;
+            AddLogsToRules();
+        }
+
+        /// <summary>
+        /// Builds list of rules. 
+        /// TODO : Caon move rules to persistence layer and instantiate via ClassName. 
+        /// </summary>
+        private void CompileRules()
+        {
+            _passwordCheckRules.Add(new PasswordLengthMaxRule());
+
+            _loginRules.Add(new CheckBlackListRule(requestData));
+            _loginRules.Add(new BruteForceIdentificationRule(requestData));
+            _loginRules.Add(new UserAccountLockRule());
+            _loginRules.Add(new PasswordMatchRule());           
+        }
+
+        /// <summary>
+        /// Adds log objects to rules 
+        /// </summary>
+        private void AddLogsToRules()
+        { 
+            foreach (AbstractRule rule in _passwordCheckRules)
+            {
+                rule.Log = this.Logger;
+            }
+            foreach (AbstractRule rule in _loginRules)
+            {
+                rule.Log = this.Logger;
+            }
+        }
+
+        /// <summary>
+        /// Performs password validation checks using rules 
+        /// </summary>
+        /// <param name="password"></param>
+        /// <returns></returns>
+        public Message CheckPasswordAgainstRules(string password, int locationID)
+        {
+            try
+            {
+                AuthorizationConfig config = ConfigService.Instance.GetConfigForLocationID(locationID);
+                return EvaluateRules(_passwordCheckRules, "", password, config, null);
+            }
+            catch(Exception ex)
+            {
+                // TODO : Some logging would be nice 
+
+                return new Message
+                {
+                    Result = MessageResult.ERROR,
+                    Text = "Error: " + ex.Message
+                };
+            }
+        }
 
         /// <summary>
         /// Evaluates all required login rules on the given username and password. 
+        /// User login attempts are resetted if rules evaluation returns as successful. 
         /// Note: We are relying on getting the Location ID from the user object in this case. 
-        /// </summary>
-        /// <param name="username"></param>
-        /// <param name="password"></param>
+        /// </summary> 
         /// <returns></returns>
         public Message HandleLogin(string username, string password)
         {
@@ -42,7 +112,14 @@ namespace MyHealthPassAuth.RuleEngine
                     };
                 }
                 AuthorizationConfig config = ConfigService.Instance.GetConfigForLocationID(user.LocationID);
-                return EvaluateRules(_loginAuthRules, username, password, config); 
+                Message message = EvaluateRules(_loginRules, username, password, config, user); 
+                if (message.Result == MessageResult.SUCCESS)
+                {
+                    DataService.Instance.ResetLoginAttempts(user);
+                }
+                DataService.Instance.AddAuthenticationLog(requestData.IpAddress, 
+                    requestData.Request, requestData.UserAgent, message.Text);
+                return message;
             }
             catch(Exception ex)
             {
@@ -65,13 +142,13 @@ namespace MyHealthPassAuth.RuleEngine
         /// <param name="password"></param>
         /// <param name="configs"></param>
         /// <returns></returns>
-        private Message EvaluateRules(List<IAuthRule> rules, string username, string password, AuthorizationConfig configs)
+        private Message EvaluateRules(List<IAuthRule> rules, string username, string password, AuthorizationConfig configs, User user)
         {
             try
             { 
                 foreach (IAuthRule rule in rules)
                 {
-                    Message message = rule.EvaluateRule(username, password, configs);
+                    Message message = rule.EvaluateRule(username, password, configs, user);
                     if (message.Result == MessageResult.ERROR)
                     {
                         return message; 
--- MyHealthPassAuthTest/RulesTest/RulesHandlerTest.cs
+++ MyHealthPassAuthTest/RulesTest/RulesHandlerTest.cs
@@ -14,12 +14,21 @@ namespace MyHealthPassAuthTest.RulesTest
     [TestClass]
     public class RulesHandlerTest : AbstractRepositoryTest
     {
+        private RequestData requestData; 
+
         [TestInitialize]
         public void TestInitialize()
         {
             Initialize();
             DataService.Instance.Initialize(this.testDbContext);
             ConfigService.Instance.Initialize(this.testDbContext);
+
+            requestData = new RequestData
+            {
+                Request = "some.request",
+                IpAddress = "192.68.1.1",
+                UserAgent = "some.user.agent"
+            }; 
         }
 
         [TestCleanup]
@@ -39,7 +48,7 @@ namespace MyHealthPassAuthTest.RulesTest
                 1,
                 new DateTime(2017, 12, 9, 0, 0, 0));
 
-            RulesHandler rulesHandler = new RulesHandler();
+            RulesHandler rulesHandler = new RulesHandler(requestData);
             Message message = rulesHandler.HandleLogin("keshav.bahadoor", "123456");
 
             Assert.AreEqual(MessageResult.ERROR, message.Result);
@@ -47,24 +56,36 @@ namespace MyHealthPassAuthTest.RulesTest
         }
 
         [TestMethod]
-        public void TestCheckPasswordAgainstRules()
+        public void TestCheckPasswordAgainstRulesError()
         {
             TestUtils.AddAuthConfig(testDbContext, 1, 10, 5, 1, 1, 1, 1, 10000, 3, 13, 600, 1200, 1);
             ConfigService.Instance.LoadConfigurationList();
-
-            TestUtils.AddUserToRepository(testDbContext,
-                1,
-                "test.user",
-                "123456",
-                0,
-                1,
-                new DateTime(2017, 12, 9, 0, 0, 0));            
-
-            RulesHandler rulesHandler = new RulesHandler();
+            
+            RulesHandler rulesHandler = new RulesHandler(requestData);
             Message message = rulesHandler.CheckPasswordAgainstRules("123456789awer", 1);
 
             Assert.AreEqual(MessageResult.ERROR, message.Result);
             Assert.AreEqual("Password length is too long", message.Text);
+        }
+
+        [TestMethod]
+        public void TestSuccesfulLogin()
+        {
+            TestUtils.AddUserToRepository(testDbContext,
+               1,
+               "test.user",
+               "123456",
+               0,
+               1,
+               new DateTime(2017, 12, 9, 0, 0, 0));
+
+            TestUtils.AddAuthConfig(testDbContext, 1, 10, 5, 1, 1, 1, 1, 10000, 3, 13, 600, 1200, 1);
+            ConfigService.Instance.LoadConfigurationList();
+
+            RulesHandler rulesHandler = new RulesHandler(requestData);
+            Message message = rulesHandler.HandleLogin("test.user", "123456");
+
+            Assert.AreEqual(MessageResult.SUCCESS, message.Result);
         }
 
         
