06c3d7c1928f3aacf947c1b642e4651ccc249128
Keshav Bahadoor <keshav.bahadoor@gmail.com>
2017-12-11 12:09:30 -0400

    Added comments

--- MyHealthPassAuth/Entities/AuthorizationConfig.cs
+++ MyHealthPassAuth/Entities/AuthorizationConfig.cs
@@ -33,15 +33,27 @@ namespace MyHealthPassAuth.Entities
         [Column("max_user_session_seconds")]
         public int MaxUserSessionSeconds { get; set; }
 
+        /// <summary>
+        /// Amount of attempts before a user account is to be locked 
+        /// </summary>
         [Column("login_account_lock_attempts")]
         public int LoginAccountLockAttempts { get; set; }
 
+        /// <summary>
+        /// Amount of identified brute force attempts before client is blacklisted 
+        /// </summary>
         [Column("brute_force_block_attempts")]
         public int BruteForceBlockAttempts { get; set; }
 
+        /// <summary>
+        /// Length of time in seconds to satisfy the criteria of an identified brute force attempt
+        /// </summary>
         [Column("brute_force_identification_seconds")]
         public int BruteForceIdentificationSeconds { get; set; }
 
+        /// <summary>
+        /// Length of time in seconds that a client is blocked from accessing the system 
+        /// </summary>
         [Column("brute_force_block_seconds")]
         public int BruteForceBlockSeconds { get; set; }
 
